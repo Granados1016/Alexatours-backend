21fdc492973cf6db6f7fce2566c494dd97e9467f
Granados1016 <jesser.granados@gmail.com>
2026-08-13 22:29:24 -0600

    fix: agregar alexatours-frontend.vercel.app a CORS

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:3001',
       'http://localhost:3000',
+      'https://alexatours-frontend.vercel.app',
       process.env.FRONTEND_URL,
     ].filter(Boolean),
     credentials: true,
